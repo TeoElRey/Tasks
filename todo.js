ab9a7b214017bff0dcb0ffd0f6b75a6e0285d78b
TeoElRey <teodortopancs@gmail.com>
2022-12-12 21:05:28 -0500

    updated script

--- todo.js
+++ todo.js
@@ -1,29 +1,54 @@
-const events = [{
-  title: 'Eat',
-  dueDate: '2022-12-12',
-}, {
-  title: 'Web Development',
-  dueDate: '2023-1-1',
-}, {
-  title: 'Exercise',
-  dueDate: '2078-8-4'
-}];
-
-render();
-
-function addEvent() {
-  const textbox = document.getElementById('input');
-  const title = textbox.value;
-  const datePicker = document.getElementById('date');
-  const dueDate = datePicker.value;
-  events.push({
-    title: title,
-    dueDate: dueDate
-  });
-
-  render();
+// model section
+let events;
+const savedEvents = JSON.parse(localStorage.getItem('tasks'));
+if((Array.isArray(savedEvents))) {
+  events = savedEvents;
+} else {
+  events = [{
+    title: 'Eat',
+    dueDate: '2022-12-12',
+    id: 'id1',
+  }, {
+    title: 'Web Development',
+    dueDate: '2023-1-1',
+    id: 'id2',
+  }, {
+    title: 'Exercise',
+    dueDate: '2078-8-4',
+    id: 'id3'
+  }];
 }
 
+render();
+// creates a todo
+function createTodo(title, dueDate, id) {
+  events.push({
+    title: title,
+    dueDate: dueDate,
+    id: id,
+  });
+
+  saveList();
+}
+
+// deletes a todo
+function deleteTodo(idToDelete) {
+  events = events.filter(function (todo) {
+    if(todo.id === idToDelete) {
+      return false;
+    } else {
+      return true;
+    }
+  });
+
+  saveList();
+}
+
+function saveList() {
+  localStorage.setItem('tasks', JSON.stringify(events));
+}
+
+// view section
 function render() {
   document.getElementById('todo-list').innerHTML='';
 
@@ -32,8 +57,29 @@ function render() {
     element.innerText = events.title + ' ' + events.dueDate;
     const deleteButton = document.createElement('button');
     deleteButton.innerText ='Delete';
+    deleteButton.style='margin-left: 12px';
+    deleteButton.onclick = deleteEvent;
+    deleteButton.id = events.id;
     element.appendChild(deleteButton);
     const todoList = document.getElementById('todo-list');
     todoList.appendChild(element)
   });
+}
+// controller section
+function addEvent() {
+  const textbox = document.getElementById('input');
+  const title = textbox.value;
+  const datePicker = document.getElementById('date');
+  const dueDate = datePicker.value;
+  const id = '' + new Date().getTime;
+  createTodo(title, dueDate, id);
+  render();
+}
+
+function deleteEvent(event) {
+  const deleteButton = event.target;
+  const idToDelete = deleteButton.id;
+
+  deleteTodo(idToDelete);
+  render();
 }
